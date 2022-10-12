a95231167eae47306fc01773449b84bdef1713d2
Sezi <sezimka1205@gmail.com>
2022-10-12 15:38:05 -0400

    registration page

--- src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
+++ src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
@@ -4,8 +4,13 @@ import com.cydeo.employeeregistrationproject.bootstrap.DataGenerator;
 import com.cydeo.employeeregistrationproject.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -21,13 +26,16 @@ public class EmployeeController {
     }
 
 
-    @GetMapping("/list")
-    public String listEmployee(Model model){
+    @PostMapping("/list")
+    public String listEmployee(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model){
 
-        model.addAttribute("employee", new Employee());
-        model.addAttribute("states", DataGenerator.getAllStates());
-
-        return "employee/employee-create";
+        if(bindingResult.hasErrors()){
+            model.addAttribute("states",DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
+        DataGenerator.saveEmployee(employee);
+        model.addAttribute("employees",DataGenerator.readAllEmployees());
+        return "employee/employee-list";
     }
 
 }
--- src/main/java/com/cydeo/employeeregistrationproject/model/Employee.java
+++ src/main/java/com/cydeo/employeeregistrationproject/model/Employee.java
@@ -1,10 +1,11 @@
 package com.cydeo.employeeregistrationproject.model;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.Date;
 @Data
@@ -12,8 +13,17 @@ import java.util.Date;
 @AllArgsConstructor
 @ToString
 public class Employee {
+
+//    @NotNull  // can't be null
+//    @NotEmpty // can't be null and ""
+//    @NotBlank // can't be null and "" and " "
+
+    @NotBlank
+    @Size(max = 12,min = 2)
     private String firstName;
     private String lastName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
     private String email;
     private String password;
